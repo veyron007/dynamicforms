b1e9d9464d4185d427651f3e6fb9d5463a56bca4
Varun Pokhriyal <varun1997.pokhriyal@gmail.com>
2023-06-24 23:41:53 +0530

    added support for dynamic css, needs refining

--- src/app/components/FormConfig.tsx
+++ src/app/components/FormConfig.tsx
@@ -26,484 +26,12 @@ import InputLabel from "@mui/material/InputLabel";
 // import control from "@mui/material";
 // import FormControl from '@mui/material/FormControl';
 // import FormLabel from '@mui/material/FormLabel';
-
-
+import formData from "../../../public/formData.json";
 
 export default function FormConfig() {
   const form = useForm();
   const { register, control } = form;
-  //   const [telephone, setTelephone] = useState("");
-  const [formData] = useState([
-    [
-      {
-        type: "text",
-        label: "First Name",
-        name: "firstName",
-        required: true,
-        validation: { maxLength: 20, minLength: 3 },
-        options: [],
-      },
-      {
-        type: "text",
-        label: "Last Name",
-        name: "lastName",
-        required: true,
-        validation: { maxLength: 20, minLength: 3 },
-        options: [],
-      },
-      {
-        type: "email",
-        label: "Email",
-        name: "email",
-        required: true,
-        options: [],
-      },
-      {
-        type: "password",
-        label: "Password",
-        name: "password",
-        required: true,
-        validation: { maxLength: 20, minLength: 6 },
-        options: [],
-      },
-    ],
-    [
-      {
-        type: "text",
-        label: "Full Name",
-        name: "fullName",
-        required: true,
-        validation: { maxLength: 20, minLength: 3 },
-        options: [],
-      },
-      {
-        type: "select",
-        label: "Favorite color",
-        name: "favoriteColor",
-        options: [
-          { label: "Red", value: "red" },
-          { label: "Blue", value: "blue" },
-          { label: "Green", value: "green" },
-        ],
-        required: true,
-      },
-      {
-        type: "radio",
-        label: "Gender",
-        name: "gender",
-        options: [
-          { label: "Male", value: "male" },
-          { label: "Female", value: "female" },
-          { label: "Other", value: "other" },
-        ],
-        required: true,
-      },
-      {
-        type: "checkbox",
-        label: "I agree to the terms and conditions",
-        name: "terms",
-        required: true,
-        options: [],
-      },
-    ],
-    [
-      {
-        type: "text",
-        label: "First Name",
-        name: "firstName",
-        required: true,
-        validation: { maxLength: 20, minLength: 3 },
-        options: [],
-      },
-      {
-        type: "text",
-        label: "Last Name",
-        name: "lastName",
-        required: true,
-        validation: { maxLength: 20, minLength: 3 },
-        options: [],
-      },
-      {
-        type: "tel",
-        label: "Phone Number",
-        name: "phoneNumber",
-        required: true,
-        options: [],
-        validation: {
-          minLength: 2,
-          maxLength: 15,
-          pattern: "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$",
-        },
-      },
-      {
-        type: "text",
-        label: "Address",
-        name: "address",
-        required: true,
-        options: [],
-      },
-    ],
-    [
-      {
-        type: "text",
-        label: "First Name",
-        name: "firstName",
-        required: true,
-        validation: { maxLength: 20, minLength: 3 },
-        options: [],
-      },
-      {
-        type: "text",
-        label: "Last Name",
-        name: "lastName",
-        required: true,
-        validation: { maxLength: 20, minLength: 3 },
-        options: [],
-      },
-      {
-        type: "email",
-        label: "Email",
-        name: "email",
-        required: true,
-        options: [],
-      },
-      {
-        type: "password",
-        label: "Password",
-        name: "password",
-        required: true,
-        validation: { maxLength: 20, minLength: 6 },
-        options: [],
-      },
-      {
-        type: "password",
-        label: "Confirm Password",
-        name: "confirmPassword",
-        required: true,
-        validation: { maxLength: 20, minLength: 6 },
-        options: [],
-      },
-    ],
-    [
-      {
-        type: "text",
-        label: "Username",
-        name: "username",
-        required: true,
-        validation: { maxLength: 20, minLength: 3 },
-        options: [],
-      },
-      {
-        type: "email",
-        label: "Email",
-        name: "email",
-        required: true,
-        options: [],
-      },
-      {
-        type: "password",
-        label: "Password",
-        name: "password",
-        required: true,
-        validation: { maxLength: 20, minLength: 6 },
-        options: [],
-      },
-      {
-        type: "password",
-        label: "Confirm Password",
-        name: "confirmPassword",
-        required: true,
-        validation: { maxLength: 20, minLength: 6 },
-        options: [],
-      },
-    ],
-
-    [
-      {
-        type: "text",
-        label: "Full Name",
-        name: "fullName",
-        required: true,
-        options: [],
-      },
-      {
-        type: "email",
-        label: "Email",
-        name: "email",
-        required: true,
-        options: [],
-      },
-      {
-        type: "tel",
-        label: "Phone Number",
-        name: "phoneNumber",
-        required: true,
-        options: [],
-        validation: {
-          minLength: 2,
-          maxLength: 15,
-          pattern: "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$",
-        },
-      },
-      {
-        type: "text",
-        label: "Subject",
-        name: "subject",
-        required: true,
-        options: [],
-      },
-      {
-        type: "textarea",
-        label: "Message",
-        name: "message",
-        required: true,
-        options: [],
-      },
-    ],
-    [
-      {
-        type: "text",
-        label: "Full Name",
-        name: "fullName",
-        required: true,
-        options: [],
-      },
-      {
-        type: "email",
-        label: "Email",
-        name: "email",
-        required: true,
-        options: [],
-      },
-      {
-        type: "tel",
-        label: "Phone Number",
-        name: "phoneNumber",
-        required: true,
-        options: [],
-        validation: {
-          minLength: 2,
-          maxLength: 15,
-          pattern: "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$",
-        },
-      },
-      {
-        type: "textarea",
-        label: "Cover Letter",
-        name: "coverLetter",
-        required: true,
-        options: [],
-      },
-      {
-        type: "file",
-        label: "Resume/CV",
-        name: "resume",
-        required: true,
-        options: [],
-      },
-    ],
-    [
-      {
-        type: "text",
-        label: "Full Name",
-        name: "fullName",
-        required: true,
-        options: [],
-      },
-      {
-        type: "email",
-        label: "Email",
-        name: "email",
-        required: true,
-        options: [],
-      },
-      {
-        type: "tel",
-        label: "Phone Number",
-        name: "phoneNumber",
-        required: true,
-        options: [],
-        validation: {
-          minLength: 2,
-          maxLength: 15,
-          pattern: "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$",
-        },
-      },
-      {
-        type: "select",
-        label: "Service/Department",
-        name: "serviceDepartment",
-        options: [
-          { label: "Customer Service", value: "customerService" },
-          { label: "IT Support", value: "itSupport" },
-          { label: "Product Development", value: "productDevelopment" },
-          { label: "Other", value: "other" },
-        ],
-        required: true,
-      },
-      {
-        type: "textarea",
-        label: "Message",
-        name: "message",
-        required: true,
-        options: [],
-      },
-    ],
-    [
-      {
-        type: "text",
-        label: "Full Name",
-        name: "fullName",
-        required: true,
-        options: [],
-      },
-      {
-        type: "email",
-        label: "Email",
-        name: "email",
-        required: true,
-        options: [],
-      },
-      {
-        type: "tel",
-        label: "Phone Number",
-        name: "phoneNumber",
-        required: true,
-        options: [],
-        validation: {
-          minLength: 2,
-          maxLength: 15,
-          pattern: "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$",
-        },
-      },
-      {
-        type: "text",
-        label: "Event Name",
-        name: "eventName",
-        required: true,
-        options: [],
-      },
-      {
-        type: "date",
-        label: "Event Date",
-        name: "eventDate",
-        required: true,
-        options: [],
-      },
-    ],
-    [
-      {
-        type: "text",
-        label: "Full Name",
-        name: "fullName",
-        required: true,
-        options: [],
-      },
-      {
-        type: "email",
-        label: "Email",
-        name: "email",
-        required: true,
-        options: [],
-      },
-      {
-        type: "tel",
-        label: "Phone Number",
-        name: "phoneNumber",
-        required: true,
-        options: [],
-        validation: {
-          minLength: 2,
-          maxLength: 15,
-          pattern: "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$",
-        },
-      },
-      {
-        type: "radio",
-        label: "How did you hear about us?",
-        name: "heardAboutUs",
-        options: [
-          { label: "Search engine", value: "searchEngine" },
-          { label: "Social media", value: "socialMedia" },
-          { label: "Word of mouth", value: "wordOfMouth" },
-          { label: "Other", value: "other" },
-        ],
-        required: true,
-      },
-      {
-        type: "checkbox",
-        label:
-          "Which of the following products/services have you used or are interested in?",
-        name: "interests",
-        required: true,
-        options: [
-          { label: "Product A", value: "productA" },
-          { label: "Product B", value: "productB" },
-          { label: "Service A", value: "serviceA" },
-          { label: "Service B", value: "serviceB" },
-        ],
-      },
-    ],
-    [
-      {
-        type: "text",
-        label: "Full Name",
-        name: "fullName",
-        required: true,
-        options: [],
-      },
-      {
-        type: "email",
-        label: "Email",
-        name: "email",
-        required: true,
-        options: [],
-      },
-      {
-        type: "tel",
-        label: "Phone Number",
-        name: "phoneNumber",
-        required: true,
-        options: [],
-        validation: {
-          minLength: 2,
-          maxLength: 15,
-          pattern: "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$",
-        },
-      },
-      {
-        type: "text",
-        label: "Course Name",
-        name: "courseName",
-        required: true,
-        options: [],
-      },
-      {
-        type: "number",
-        label: "Student ID",
-        name: "studentId",
-        required: true,
-        options: [],
-      },
-      {
-        type: "text",
-        label: "Professor Name",
-        name: "professorName",
-        required: true,
-        options: [],
-      },
-      {
-        type: "textarea",
-        label: "Question/Comment",
-        name: "questionComment",
-        required: true,
-        options: [],
-      },
-    ],
-  ]);
-
-
-  
+ 
 
   const SubmitHandler = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
@@ -511,6 +39,8 @@ export default function FormConfig() {
     const data = Object.fromEntries(formData.entries());
     console.log(data);
   };
+
+
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentForm, setCurrentForm] = useState(formData[currentIndex]);
@@ -572,9 +102,22 @@ export default function FormConfig() {
         autoComplete="off"
       >
         {currentForm?.map(
-          ({ type, label, name, required, validation, options }) => {
+          ({ type, label, name, required, validation, options, css  }) => {
+          
+            // interface csstype {
+            //   name: string;
+            //   value: string;
+            // }
+            let obj: any = {
+                name:'',
+                value:''
+            }
+            css.forEach(function(el:any){
+              let key = el.name
+              obj[key as keyof any]= el.value
+          })
             return (
-              <>
+              <div key={label}>
                 {type == "text" || type == "email" ? (
                   <TextField
                     id="outlined-basic"
@@ -582,6 +125,7 @@ export default function FormConfig() {
                     label={label}
                     required={required}
                     variant="outlined"
+                    sx={{...obj}}
                   />
                 ) : type == "textarea" ? (
                   <TextField
@@ -593,6 +137,7 @@ export default function FormConfig() {
                     maxRows={4}
                     defaultValue=""
                     variant="standard"
+                    sx={{...obj}}
                   />
                 ) : type == "password" ? (
                   <TextField
@@ -601,6 +146,7 @@ export default function FormConfig() {
                     name={name}
                     required={required}
                     type="password"
+                    sx={{...obj}}
                   />
                 ) : type == "date" ? (
                   <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -617,6 +163,7 @@ export default function FormConfig() {
                     label={label}
                     required={required}
                     variant="outlined"
+                    sx={{...obj}}
                   />
                 ) : type == "number" ? (
                   <TextField
@@ -625,18 +172,19 @@ export default function FormConfig() {
                     required={required}
                     label={label}
                     variant="standard"
+                    sx={{...obj}}
                   />
                 ) : type == "file" ? (
-                    <TextField
-                      id="outlined-basic"
-                      name={name}
+                  <TextField
+                    id="outlined-basic"
+                    name={name}
                     //   label={label}
-                      type="file"
-                      required={required}
-                      variant="outlined"
-                    />
-                  ) :
-                type == "select" ? (
+                    type="file"
+                    required={required}
+                    variant="outlined"
+                    sx={{...obj}}
+                  />
+                ) : type == "select" ? (
                   <>
                     <InputLabel id="demo-simple-select-label">
                       {label}
@@ -648,6 +196,7 @@ export default function FormConfig() {
                       required={required}
                       value={color}
                       onChange={handleChange}
+                      sx={{...obj}}
                     >
                       {options?.map((option) => (
                         <MenuItem value={option.value}>{option.label}</MenuItem>
@@ -663,6 +212,7 @@ export default function FormConfig() {
                       name={name}
                       value={selectedOption}
                       onChange={handleOptionChange}
+                      sx={{...obj}}
                     >
                       {options?.map((option) => (
                         <FormControlLabel
@@ -670,6 +220,7 @@ export default function FormConfig() {
                           value={option.value}
                           control={<Radio />}
                           label={option.label}
+                          sx={{...obj}}
                         />
                       ))}
                     </RadioGroup>
@@ -679,15 +230,16 @@ export default function FormConfig() {
                     control={<Checkbox required={required} />}
                     label={label}
                     name={name}
+                    sx={{...obj}}
                   />
                 ) : (
                   ""
                 )}
-              </>
+              </div>
             );
           }
         )}
-        <Button type="submit" variant="contained" color="primary">
+        <Button type="submit" variant="contained" color="primary" >
           SUBMIT
         </Button>
         {/* <DevTool control={control} /> */}
