e7fcce69588a34ea223f7931465fb18e2baa0085
Varun Pokhriyal <varun1997.pokhriyal@gmail.com>
2023-06-27 01:02:58 +0530

    Validations part 1

--- src/app/components/DynamicForm.tsx
+++ src/app/components/DynamicForm.tsx
@@ -1,5 +1,6 @@
 "use client";
-// for now see this code as the base of dynamic forms. Suggest me changes and also prepare a general schema for me for next operation
+import { useState } from "react";
+import { useEffect } from "react";
 import {
   TextField,
   Button,
@@ -10,28 +11,30 @@ import {
   FormControlLabel,
   Checkbox,
 } from "@mui/material";
-
-import {Form, Field, ErrorMessage, Formik, useFormik } from 'formik';
-
 import dayjs from "dayjs";
 import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
+import { useFormik, Formik } from "formik";
+import * as Yup from "yup";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { MenuItem } from "@mui/material";
 import InputLabel from "@mui/material/InputLabel";
 import formData from "../../../public/formData.json";
 
-import * as Yup from "yup";
+const DynamicForm = () => {
+  const [currentFormIndex, setCurrentFormIndex] = useState(0);
+  const [currentFormData, setCurrentFormData] = useState(formData);
 
-export default function FormConfig() {
-  const form = useForm();
-  const { register, control } = form;
+  const setCurrentForm = (index: any) => {
+    setCurrentFormIndex(index);
+  };
+  const currentForm = currentFormData[currentFormIndex];
+
 
   const validationSchema = Yup.object().shape({
+    // Define your validation schema based on the form configuration
     firstName: Yup.string()
       .required("First Name is required")
       .min(3, "First Name must be at least 3 characters")
@@ -45,69 +48,32 @@ export default function FormConfig() {
       .required("Password is required")
       .min(6, "Password must be at least 6 characters")
       .max(20, "Password cannot exceed 20 characters"),
-    confirmPassword: Yup.string()
-      .required("Confirm your password")
-      .oneOf([Yup.ref("password")], "Password does not match"),
   });
 
-  // const SubmitHandler = (event: React.FormEvent<HTMLFormElement>): void => {
-  //   event.preventDefault();
-  //   const formData = new FormData(event.currentTarget);
-  //   const data = Object.fromEntries(formData.entries());
-  //   console.log(data);
-  // };
   const formik = useFormik({
-    initialValues: {},
-    validationSchema,
-    onSubmit: (values : any) => {
-      console.log(values);
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values, null, 2));
     },
   });
-  
-  
- // const { getFieldProps, errors } = formik;
- // const SubmitHandler = formik.handleSubmit;
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentForm, setCurrentForm] = useState(formData[currentIndex]);
-
-  const [color, setColor] = useState("");
+  
   const [selectedOption, setSelectedOption] = useState("");
 
   const handleChange = (event: SelectChangeEvent) => {
-    setColor(event.target.value as string);
+    formik.setFieldValue(event.target.name, event.target.value);
   };
 
   const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedOption((event.target as HTMLInputElement).value);
+    formik.setFieldValue(event.target.name, event.target.value);
   };
-
-  const SubmitHandler = () => {
-    const handleSubmit = (values :any, { setSubmitting = Boolean }) => {
-      // Perform any asynchronous operations, such as API calls, here
-    
-      // Simulating an API call with a setTimeout
-      setTimeout(() => {
-        // Once the operation is complete, you can access the form values
-        console.log(values);
-    
-        // Reset any form submission states
-        setSubmitting(false);
-    
-        // Optionally, perform any other actions after form submission
-        // For example, redirecting to a different page
-        // history.push('/success'); // Assuming you have access to the 'history' object
-      }, 1000); // Simulating a delay of 1 second (adjust as needed)
-    };
-    
-  };
-  
-  useEffect(() => {
-    setCurrentForm(formData[currentIndex]);
-  }, [currentIndex]);
 
   return (
-   <>
+    <Stack>
       <Box
         component="div"
         sx={{
@@ -116,37 +82,19 @@ export default function FormConfig() {
           alignItems: "center",
         }}
       >
-        {Array(formData.length)
-          .fill(0)
-          .map((el, index) => {
-            return (
-              <Button
-                key={index}
-                variant="contained"
-                onClick={() => {
-                  setCurrentIndex(index);
-                }}
-                color="success"
-                sx={{ marginRight: "2px" }}
-              >
-                {index + 1}
-              </Button>
-            );
-          })}
+        {currentFormData.map((_, index) => (
+          <Button
+            key={index}
+            variant="contained"
+            onClick={() => setCurrentForm(index)}
+            color="success"
+            sx={{ marginRight: "2px" }}
+          >
+            {index + 1}
+          </Button>
+        ))}
       </Box>
-    
-     <Form  onSubmit={SubmitHandler}
-        style={{
-         
-          flexDirection: "column",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-       >
-      {/* Rest of your form components */}
-    
-      {/* <Box
+      <Box
         component="form"
         onSubmit={formik.handleSubmit}
         sx={{
@@ -158,63 +106,65 @@ export default function FormConfig() {
         }}
         noValidate
         autoComplete="off"
-      > */}
-
-       {/* let obj: any = {
+      >
+        {currentForm?.map(
+          ({ type, label, name, required, validation, options, css }) => {
+            let obj: any = {
               name: "",
               value: "",
             };
             css.forEach(function (el: any) {
               let key = el.name;
               obj[key as keyof any] = el.value;
-            }); */}
-
-        {currentForm?.map(
-          ({ type, label, name, required, validation, options }) => {
-           
+            });
             return (
               <div key={label}>
                 {type == "text" || type == "email" ? (
-                  <>
-                    {" "}
-                    <TextField
-                      id="outlined-basic"
-                      label={label}
-                      name={name}
-                      variant="outlined"
-                      // value={formik.values[name]}
-                      // onChange={formik.handleChange}
-                      // error={formik.touched[name] && Boolean(formik.errors[name])}
-                      // helperText={formik.touched[name] && formik.errors[name]}
-                      // sx={{ ...obj }}
-                    />
-                    <ErrorMessage name="name" />
-                  </>
+                  <TextField
+                    id="outlined-basic"
+                    {...formik.getFieldProps(name)}
+                    label={label}
+                    required={true}
+                    variant="outlined"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                    sx={{
+                      ...obj,
+                    }}
+                  />
                 ) : type == "textarea" ? (
-                  <>
                   <TextField
                     id="standard-multiline-static"
-                    name={name}
+                    {...formik.getFieldProps(name)}
                     required={true}
                     label={label}
                     multiline
                     maxRows={4}
                     defaultValue=""
                     variant="standard"
-                    // sx={{ ...obj }}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                    sx={{
+                      ...obj,
+                    }}
                   />
-                  </>
                 ) : type == "password" ? (
-                  <>
                   <TextField
                     id="outlined-password-input"
+                    {...formik.getFieldProps(name)}
                     label={label}
-                    name={name}
                     required={true}
                     type="password"
-                    // sx={{ ...obj }}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                    sx={{
+                      ...obj,
+                    }}
                   />
-                  </>
                 ) : type == "date" ? (
                   <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DemoContainer components={["DatePicker"]}>
@@ -224,34 +174,47 @@ export default function FormConfig() {
                     </DemoContainer>
                   </LocalizationProvider>
                 ) : type == "tel" ? (
-                  <>
                   <TextField
                     id="outlined-basic"
-                    name={name}
+                    {...formik.getFieldProps(name)}
                     label={label}
                     required={true}
                     variant="outlined"
-                    // sx={{ ...obj }}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                    sx={{
+                      ...obj,
+                    }}
                   />
-                  </>
                 ) : type == "number" ? (
                   <TextField
                     id="outlined-basic"
-                    name={name}
+                    {...formik.getFieldProps(name)}
                     required={true}
                     label={label}
                     variant="standard"
-                    // sx={{ ...obj }}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                    sx={{
+                      ...obj,
+                    }}
                   />
-                ) : type == "file" ? (
+                ) : type === "file" ? (
                   <TextField
                     id="outlined-basic"
-                    name={name}
+                    {...formik.getFieldProps(name)}
                     //   label={label}
                     type="file"
                     required={true}
                     variant="outlined"
-                    // sx={{ ...obj }}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                    sx={{
+                      ...obj,
+                    }}
                   />
                 ) : type == "select" ? (
                   <>
@@ -261,14 +224,16 @@ export default function FormConfig() {
                     <Select
                       id="demo-simple-select"
                       label={label}
-                      name={name}
+                      {...formik.getFieldProps(name)}
                       required={true}
-                      value={color}
-                      onChange={handleChange}
-                      // sx={{ ...obj }}
+                      sx={{
+                        ...obj,
+                      }}
                     >
                       {options?.map((option) => (
-                        <MenuItem value={option.value}>{option.label}</MenuItem>
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.label}
+                        </MenuItem>
                       ))}
                     </Select>
                   </>
@@ -281,7 +246,9 @@ export default function FormConfig() {
                       name={name}
                       value={selectedOption}
                       onChange={handleOptionChange}
-                      // sx={{ ...obj }}
+                      sx={{
+                        ...obj,
+                      }}
                     >
                       {options?.map((option) => (
                         <FormControlLabel
@@ -289,20 +256,22 @@ export default function FormConfig() {
                           value={option.value}
                           control={<Radio />}
                           label={option.label}
-                          // sx={{ ...obj }}
+                          sx={{
+                            ...obj,
+                          }}
                         />
                       ))}
                     </RadioGroup>
                   </>
                 ) : type == "checkbox" ? (
-                  <>
                   <FormControlLabel
-                    control={<Checkbox required={true} />}
+                    control={<Checkbox required={required} />}
                     label={label}
                     name={name}
-                    // sx={{ ...obj }}
+                    sx={{
+                      ...obj,
+                    }}
                   />
-                  </>
                 ) : (
                   ""
                 )}
@@ -311,12 +280,11 @@ export default function FormConfig() {
           }
         )}
         <Button type="submit" variant="contained" color="primary">
-          SUBMIT
+          Submit
         </Button>
-     
-      </Form>
-      
-    </>
-   
+      </Box>
+    </Stack>
   );
-}
+};
+
+export default DynamicForm;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import DynamicForm from "./components/DynamicForm"
 import FormConfig from "./components/FormConfig"
+import LatestForm from "./components/LatestForm"
+
 
 export default function Home() {
   return (
     <div>
-      <FormConfig />
+      {/* <FormConfig /> */}
+      {/* <DynamicForm /> */}
+      < LatestForm />
+
     </div>
     
   )
 }
+ // "validation": {
+            //     "minLength": 2,
+            //     "maxLength": 15,
+            //     "pattern": "^[+]?[(]?[0-9]{3}[)]?[-\\s.]?[0-9]{3}[-\\s.]?[0-9]{4,6}$"
